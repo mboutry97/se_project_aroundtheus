95f6f84c712f3610a9d3f252353b56e2c44c2e5d
Max Boutry <maximilienboutry@gmail.com>
2024-04-18 16:05:10 -0400

    Import card elements using JS. Update img CSS

--- scripts/index.js
+++ scripts/index.js
@@ -26,19 +26,65 @@ let initialCards = [
 ];
 console.log(initialCards);
 
-/* To open profile edit modal */
+/* ----------------------- Elements ----------------------- */
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-
-profileEditButton.addEventListener("click", function () {
-  profileEditModal.classList.add("modal_opened");
-});
-
-/* To close profile edit modal */
 const profileCloseEditModalButton = document.querySelector(
   "#profile-close-edit-modal"
 );
+const profileName = document.querySelector("#profile-name");
+const profileDescription = document.querySelector("#profile-description");
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditModalSaveButton = document.querySelector(
+  "#profile-edit-modal-save"
+);
+const cardListEl = document.querySelector("#cards-list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
-profileCloseEditModalButton.addEventListener("click", function () {
+/* ----------------------- Functions ----------------------- */
+function closePopup() {
   profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true); /* cloning the template */
+  const cardImageEl = cardElement.querySelector("#card-image");
+  const cardTitleEl = cardElement.querySelector("#card-title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.setAttribute("src", cardData.link);
+  cardImageEl.setAttribute("alt", cardData.name);
+  /* Add image alt text according to name of image */
+  return cardElement;
+}
+
+/* ----------------------- Event Listeners ----------------------- */
+profileEditButton.addEventListener("click", function () {
+  profileNameInput.value = profileName.textContent
+    .replace(/[\n\r]+|[\s]{2,}/g, " ")
+    .trim(); /* Pre-filling Name and removing excess whitespace */
+  profileDescriptionInput.value =
+    profileDescription.textContent; /* Pre-filling Description */
+  profileEditModal.classList.add("modal_opened"); /* Making modal visible */
+});
+
+profileCloseEditModalButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+}); /* Making modal invisible */
+
+profileEditModalSaveButton.addEventListener("click", function (event) {
+  profileName.textContent =
+    profileNameInput.value; /* Making the input apply to name */
+  profileDescription.textContent =
+    profileDescriptionInput.value; /* Making the input apply to description */
+  closePopup(); /* closing modal window when saved */
+  event.preventDefault(); /* preventing page refresh */
+});
+
+initialCards.forEach((cardData) => {
+  let cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
 });
